5e85feccf24f38f076726b9713c8c1dc695b9c9d
Supriyo <72860690+Supsource@users.noreply.github.com>
2023-01-13 20:20:31 +0530

    Create SearchUserPage.js

---
+++ src/Screens/Mainpage/SearchUserPage.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const SearchUserPage = () => {
+  return (
+    <View>
+      <Text>SearchUserPage</Text>
+    </View>
+  )
+}
+
+export default SearchUserPage
+
+const styles = StyleSheet.create({})
