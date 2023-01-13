f7222b192cb080d46c442dc61b34cf6ae6fb641b
Supriyo <72860690+Supsource@users.noreply.github.com>
2023-01-13 20:19:40 +0530

    Create MessagePage.js

---
+++ src/Screens/ChatSection/MessagePage.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const MessagePage = () => {
+  return (
+    <View>
+      <Text>MessagePage</Text>
+    </View>
+  )
+}
+
+export default MessagePage
+
+const styles = StyleSheet.create({})
