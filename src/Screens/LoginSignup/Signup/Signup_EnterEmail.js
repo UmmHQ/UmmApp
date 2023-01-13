734a853c57f1244715b93963a9c0269ab34f38e2
Supriyo <72860690+Supsource@users.noreply.github.com>
2023-01-13 20:20:07 +0530

    Create Signup_EnterEmail.js

---
+++ src/Screens/LoginSignup/Signup/Signup_EnterEmail.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Signup_EnterEmail = () => {
+  return (
+    <View>
+      <Text>Signup_EnterEmail</Text>
+    </View>
+  )
+}
+
+export default Signup_EnterEmail
+
+const styles = StyleSheet.create({})
