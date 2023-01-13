213d72591c901b9695866ec9f8c27410e2118b90
Supriyo <72860690+Supsource@users.noreply.github.com>
2023-01-14 04:14:28 +0530

    Update Signup_EnterEmail.js

--- src/Screens/LoginSignup/Signup/Signup_EnterEmail.js
+++ src/Screens/LoginSignup/Signup/Signup_EnterEmail.js
@@ -1,56 +1,85 @@
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
-
-import React from 'react'
-
-import { containerFull, goback } from '../../../CommonCss/pagecss'
-
-import { Ionicons } from '@expo/vector-icons';
-
+import { StyleSheet, Text, View, Image, TextInput, TouchableOpacity, ActivityIndicator } from 'react-native'
+import React, { useState } from 'react'
+import { containerFull, goback, hr80, logo1 } from '../../../CommonCss/pagecss'
+import logo from '../../../../assets/logo.png'
+import { formbtn, formHead, formHead2, formInput, formTextLinkCenter, formTextLinkRight } from '../../../CommonCss/formcss'
+import { MaterialIcons } from '@expo/vector-icons';
 const Signup_EnterEmail = ({ navigation }) => {
+    const [email, setEmail] = useState('')
+    const [loading, setLoading] = useState(false)
+    const handleEmail = () => {
+        // setLoading(true)
+        // navigation.navigate('Signup_EnterVerificationCode')
+        if (email == '') {
+            alert('Please enter email')
+        }
+        else {
+            setLoading(true)
+            fetch('http://10.0.2.2:3000/verify', {
+                method: 'post',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    email: email
+                })
+            })
+                .then(res => res.json()).then(
+                    data => {
+                        if (data.error === "Invalid Credentials") {
+                            // alert('Invalid Credentials')
+                            alert('Invalid Credentials')
+                            setLoading(false)
+                        }
+                        else if (data.message === "Verification Code Sent to your Email") {
+                            setLoading(false)
+                            alert(data.message);
+                            navigation.navigate('Signup_EnterVerificationCode', {
+                                useremail: data.email,
+                                userVerificationCode: data.VerificationCode
+                            })
 
+                        }
+                    }
+                )
+        }
+    }
     return (
-
         <View style={containerFull}>
+            <TouchableOpacity onPress={() => navigation.navigate('Login')} style={goback}>
 
-
-
-
-            <TouchableOpacity onPress={() => navigation.navigate("Login")} style={goback}>
-
-                <Ionicons name="arrow-back" size={24} color="gray" />
-
+                <MaterialIcons name="arrow-back-ios" size={24} color="gray" />
                 <Text style={{
-
                     color: 'gray',
-
                     fontSize: 16,
+                }}
 
-                    marginLeft: 5,
-
-                    fontWeight: 'bold'
-
-                }}>
-
-                    Go Back
-
-                </Text>
+                >Go Back</Text>
 
             </TouchableOpacity>
 
-            {/* <Text>Signup_EnterEmail</Text> */}
+            <Image source={logo} style={logo1} />
+            <Text style={formHead2}>Create a new account</Text>
+            <TextInput placeholder="Enter Your Email" style={formInput}
 
+                onChangeText={(text) => {
+                    setEmail(text)
+                }}
+            />
+            {
+                loading ?
+                    <ActivityIndicator size="large" color="white" />
+                    :
+                    <Text style={formbtn}
+                        onPress={() => handleEmail()}
+                    >
+                        Next
+                    </Text>
+            }
         </View>
-
     )
-
 }
 
-
-
-
 export default Signup_EnterEmail
-
-
-
 
 const styles = StyleSheet.create({})
