3ac67c150e6eb61805634729a5d1bc9ed93a6d4a
Supriyo <72860690+Supsource@users.noreply.github.com>
2023-01-13 20:20:43 +0530

    Create Settings1.js

---
+++ src/Screens/Settings/Settings1.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Settings1 = () => {
+  return (
+    <View>
+      <Text>Settings1</Text>
+    </View>
+  )
+}
+
+export default Settings1
+
+const styles = StyleSheet.create({})
