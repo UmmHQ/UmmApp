ca4129435c3efcaf0e1251997f8f96a892e32f9e
Supriyo <72860690+Supsource@users.noreply.github.com>
2023-01-13 20:20:27 +0530

    Create NotificationPage.js

---
+++ src/Screens/Mainpage/NotificationPage.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const NotificationPage = () => {
+  return (
+    <View>
+      <Text>NotificationPage</Text>
+    </View>
+  )
+}
+
+export default NotificationPage
+
+const styles = StyleSheet.create({})
