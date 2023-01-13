65c02e6b268174edc6e4878106463fb060128c18
Supriyo <72860690+Supsource@users.noreply.github.com>
2023-01-13 20:20:14 +0530

    Create Signup_EntreVerificationCode.js

---
+++ src/Screens/LoginSignup/Signup/Signup_EntreVerificationCode.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Signup_EntreVerificationCode = () => {
+  return (
+    <View>
+      <Text>Signup_EntreVerificationCode</Text>
+    </View>
+  )
+}
+
+export default Signup_EntreVerificationCode
+
+const styles = StyleSheet.create({})
