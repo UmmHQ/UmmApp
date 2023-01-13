d29eaf059abcdf48f101c62071f8ce5f20ad1cac
Supriyo <72860690+Supsource@users.noreply.github.com>
2023-01-13 20:20:51 +0530

    Create ChangeUsername.js

---
+++ src/Screens/Settings/ChangeUsername.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const ChangeUsername = () => {
+  return (
+    <View>
+      <Text>ChangeUsername</Text>
+    </View>
+  )
+}
+
+export default ChangeUsername
+
+const styles = StyleSheet.create({})
