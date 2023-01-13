c91c3ee451ea1325cf60a064438e95937b1ab410
Supriyo <72860690+Supsource@users.noreply.github.com>
2023-01-13 20:20:39 +0530

    Create UploadProfilePicture.js

---
+++ src/Screens/Settings/UploadProfilePicture.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const UploadProfilePicture = () => {
+  return (
+    <View>
+      <Text>UploadProfilePicture</Text>
+    </View>
+  )
+}
+
+export default UploadProfilePicture
+
+const styles = StyleSheet.create({})
