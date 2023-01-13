35fac384e1024a4b115d9bc91f3e57cdcda93352
Supriyo <72860690+Supsource@users.noreply.github.com>
2023-01-14 03:38:00 +0530

    Login

--- src/Screens/LoginSignup/Signup/Signup_EnterEmail.js
+++ src/Screens/LoginSignup/Signup/Signup_EnterEmail.js
@@ -1,14 +1,56 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+
 import React from 'react'
 
-const Signup_EnterEmail = () => {
-  return (
-    <View>
-      <Text>Signup_EnterEmail</Text>
-    </View>
-  )
+import { containerFull, goback } from '../../../CommonCss/pagecss'
+
+import { Ionicons } from '@expo/vector-icons';
+
+const Signup_EnterEmail = ({ navigation }) => {
+
+    return (
+
+        <View style={containerFull}>
+
+
+
+
+            <TouchableOpacity onPress={() => navigation.navigate("Login")} style={goback}>
+
+                <Ionicons name="arrow-back" size={24} color="gray" />
+
+                <Text style={{
+
+                    color: 'gray',
+
+                    fontSize: 16,
+
+                    marginLeft: 5,
+
+                    fontWeight: 'bold'
+
+                }}>
+
+                    Go Back
+
+                </Text>
+
+            </TouchableOpacity>
+
+            {/* <Text>Signup_EnterEmail</Text> */}
+
+        </View>
+
+    )
+
 }
 
+
+
+
 export default Signup_EnterEmail
+
+
+
 
 const styles = StyleSheet.create({})
