f877d83a08523e192498984c669c26be749e1c34
Supriyo <72860690+Supsource@users.noreply.github.com>
2023-01-14 04:15:42 +0530

    Update App.js

--- App.js
+++ App.js
@@ -18,7 +18,19 @@ import ForgotPassword_ChoosePassword from './SRC/Screens/LoginSignup/ForgotPassw
 
 import ForgotPassword_AccountRecovered from './SRC/Screens/LoginSignup/ForgotPassword/ForgotPassword_AccountRecovered';
 import Mainpage from './SRC/Screens/Mainpage/Mainpage';
-
+import All_Chats from './SRC/Screens/ChatSection/All_Chats';
+import SearchUserPage from './SRC/Screens/Mainpage/SearchUserPage';
+import NotificationPage from './SRC/Screens/Mainpage/NotificationPage';
+import My_UserProfile from './SRC/Screens/Profile/My_UserProfile';
+import Settings1 from './SRC/Screens/Settings/Settings1';
+import ChangePassword from './SRC/Screens/Settings/ChangePassword';
+import EditProfile from './SRC/Screens/Settings/EditProfile';
+import ChangeUsername from './SRC/Screens/Settings/ChangeUsername';
+import ChangeDescription from './SRC/Screens/Settings/ChangeDescription';
+import UploadProfilePicture from './SRC/Screens/Settings/UploadProfilePicture';
+import AddPost from './SRC/Screens/Mainpage/AddPost';
+import Other_UserProfile from './SRC/Screens/Profile/Other_UserProfile';
+import MessagePage from './SRC/Screens/ChatSection/MessagePage';
 const Stack = createNativeStackNavigator();
 
 export default function App() {
@@ -28,6 +40,7 @@ export default function App() {
         headerShown: false,
         animation: 'slide_from_right'
       }}>
+        <Stack.Screen name="MainPage" component={Mainpage} />
         <Stack.Screen name="Login" component={Login}
           options={{
             // animation: 'slide_from_right'
@@ -50,8 +63,39 @@ export default function App() {
         <Stack.Screen name="ForgotPassword_ChoosePassword" component={ForgotPassword_ChoosePassword} />
         <Stack.Screen name="ForgotPassword_AccountRecovered" component={ForgotPassword_AccountRecovered} />
 
+        <Stack.Screen name="All_Chats" component={All_Chats}
 
-        <Stack.Screen name="MainPage" component={Mainpage} />
+          options={{
+            animation: 'slide_from_left'
+          }}
+        />
+
+        <Stack.Screen name="SearchUserPage" component={SearchUserPage}
+
+          options={{
+            animation: 'slide_from_bottom'
+          }}
+        />
+
+        <Stack.Screen name="NotificationPage" component={NotificationPage}
+        />
+
+        <Stack.Screen name="My_UserProfile" component={My_UserProfile}
+
+          options={{
+            animation: 'slide_from_left'
+          }}
+        />
+        <Stack.Screen name="Settings_1" component={Settings1} />
+        <Stack.Screen name="ChangePassword" component={ChangePassword} />
+        <Stack.Screen name="EditProfile" component={EditProfile} />
+        <Stack.Screen name="ChangeUsername" component={ChangeUsername} />
+        <Stack.Screen name="ChangeDescription" component={ChangeDescription} />
+        <Stack.Screen name='UploadProfilePicture' component={UploadProfilePicture} />
+
+        <Stack.Screen name='AddPost' component={AddPost} />
+        <Stack.Screen name="Other_UserProfile" component={Other_UserProfile} />
+        <Stack.Screen name="MessagePage" component={MessagePage} />
       </Stack.Navigator>
     </NavigationContainer>
   );
