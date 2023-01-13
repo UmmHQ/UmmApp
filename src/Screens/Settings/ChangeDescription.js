a4b4a566d724f66785bd93f2fbc6230ea03f7b04
Supriyo <72860690+Supsource@users.noreply.github.com>
2023-01-13 20:21:02 +0530

    Create ChangeDescription.js

---
+++ src/Screens/Settings/ChangeDescription.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const ChangeDescription = () => {
+  return (
+    <View>
+      <Text>ChangeDescription</Text>
+    </View>
+  )
+}
+
+export default ChangeDescription
+
+const styles = StyleSheet.create({})
