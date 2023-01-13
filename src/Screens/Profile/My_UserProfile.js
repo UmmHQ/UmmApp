15d6e250e37e07e8980d986a87e1a90f548a565c
Supriyo <72860690+Supsource@users.noreply.github.com>
2023-01-13 20:20:36 +0530

    Create My_UserProfile.js

---
+++ src/Screens/Profile/My_UserProfile.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const My_UserProfile = () => {
+  return (
+    <View>
+      <Text>My_UserProfile</Text>
+    </View>
+  )
+}
+
+export default My_UserProfile
+
+const styles = StyleSheet.create({})
