5c7888e8b8683d3c5412f3ef9e1d58d909f8fd71
Supriyo <72860690+Supsource@users.noreply.github.com>
2023-01-13 20:20:55 +0530

    Create ChangePassword.js

---
+++ src/Screens/Settings/ChangePassword.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const ChangePassword = () => {
+  return (
+    <View>
+      <Text>ChangePassword</Text>
+    </View>
+  )
+}
+
+export default ChangePassword
+
+const styles = StyleSheet.create({})
