2230d7eb9af4d59cd37f39eae932a2de6f2cbfac
Supriyo <72860690+Supsource@users.noreply.github.com>
2023-01-13 20:19:35 +0530

    Create All_Chats.js

---
+++ src/Screens/ChatSection/All_Chats.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const All_Chats = () => {
+  return (
+    <View>
+      <Text>All_Chats</Text>
+    </View>
+  )
+}
+
+export default All_Chats
+
+const styles = StyleSheet.create({})
