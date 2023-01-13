022367268ba8a2056f20cd688826bdb1ae447101
Supriyo <72860690+Supsource@users.noreply.github.com>
2023-01-13 20:20:58 +0530

    Create Other_UserProfile.js

---
+++ src/Screens/Profile/Other_UserProfile.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Other_UserProfile = () => {
+  return (
+    <View>
+      <Text>Other_UserProfile</Text>
+    </View>
+  )
+}
+
+export default Other_UserProfile
+
+const styles = StyleSheet.create({})
