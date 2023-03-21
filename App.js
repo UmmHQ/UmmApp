f1d7aebdd96ce34b8bd118fe55d9d95bb33c0053
Supriyo <72860690+Supsource@users.noreply.github.com>
2023-03-21 20:10:57 +0530

    Updated header position in CSS file to fix overlapping issue.

--- App.js
+++ App.js
@@ -104,6 +104,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
