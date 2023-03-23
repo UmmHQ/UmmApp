f1c733f711373cbbc264f40672007d1be03abdda
Supriyo <72860690+Supsource@users.noreply.github.com>
2023-03-23 15:25:22 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -3,45 +3,31 @@ const bcrypt = require("bcrypt");
 
 
 const userSchema = new mongoose.Schema({
-
     username: {
         type: String,
         required: true
-
     },
-
     email: {
         type: String,
         required: true,
         unique: true,
-
     },
-
     password: {
         type: String,
         required: true
-
     },
-
     profilepic: {
-
         type: String,
-
         default: '',
-
         // required: true
-
     },
-
     posts: {
-
         type: Array,
         default: []
-
     }
-
 })
 
 
 // userSchema.pre('sa')
+
 mongoose.model("User", userSchema);
